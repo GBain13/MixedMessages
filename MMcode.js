3b8039851ed56f507d6fb804d9aa7b2b93a786da
Greg <greg@Gregs-MacBook-Pro.local>
2024-08-17 11:01:30 +0100

    Adds working js file and linked html

--- MMcode.js
+++ MMcode.js
@@ -1,9 +1,17 @@
 
 let youAre = ["You are Cancer", "You are Taurus", "You are Sagittarius", "You are Virgo"];
 let whichMeans = ["You are sensitive", "You are stubborn", "You are adventurous", "You are analytical"];
-let conditional = ["Unless other indicators contradict this", "Which is amplified by your other placements"];
+let conditional = ["unless other indicators contradict this", "which is amplified by your other placements"];
  
 const randomElement1 = youAre[Math.floor(Math.random() * youAre.length)];
 const randomElement2 = whichMeans[Math.floor(Math.random() * whichMeans.length)];
 const randomElement3 = conditional[Math.floor(Math.random() * conditional.length)];
-console.log(randomElement1, randomElement2, randomElement3);
+
+
+const result = `${randomElement1}. ${randomElement2}, ${randomElement3}`
+
+function printString(){
+    var string = result;
+    document.getElementById("results").innerHTML = result;
+    
+}
